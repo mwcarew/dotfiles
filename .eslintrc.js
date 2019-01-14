67f8590b2bf78dc2ebca75277cc2c31a6ddd70c8
Matthew Carew <mwcarew@gmail.com>
2019-01-14 00:17:10 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  rules: {
+    "comma-dangle": ["error", "always-multiline"],
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+  },
+};
