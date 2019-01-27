36cf96bc13f6a5e06a3d18f26fb71374039d2165
Matthew Carew <mwcarew@gmail.com>
2019-01-27 02:11:56 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:react/recommended"],
+  env: { es6: true, node: true },
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   rules: {
-    "comma-dangle": ["error", "always-multiline"],
-    semi: ["error", "always"],
-    quotes: ["error", "single"],
+    'comma-dangle': ['error', 'always-multiline'],
+    semi: ['error', 'always'],
+    quotes: ['error', 'single'],
   },
 };
