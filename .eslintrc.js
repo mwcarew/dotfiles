a81e98cf586a9b64b321b72f1c04435b5ac45e41
Matthew Carew <mwcarew@gmail.com>
2019-06-20 22:15:11 -0700

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
   env: { es6: true, node: true },
   extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parserOptions: {
+    sourceType: 'module',
+  },
   rules: {
     'comma-dangle': ['error', 'always-multiline'],
+    'react/display-name': 0,
+    'react/prop-types': 0,
     semi: ['error', 'always'],
     quotes: ['error', 'single'],
   },
