8e2be4427b0eef89953b41546dd1f0ba7a857532
Matthew Carew <mwcarew@gmail.com>
2019-11-22 02:12:58 -0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
-    globals: {
-        __PATH_PREFIX__: true,
-    },
-    extends: `react-app`,
-    rules: {
-        'comma-dangle': ['warn', 'always-multiline'],
-        quotes: ['warn', 'single'],
-        semi: ['warn', 'always']
-    },
-}
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: 'react-app',
+};
